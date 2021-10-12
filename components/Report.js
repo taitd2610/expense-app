634876a6a7c71a3f531ac89c1db2e064cc1dfbc9
taitd153 <trinhdinhtai1996@gmail.com>
2021-10-12 17:12:54 +0700

    update

--- components/Report.js
+++ components/Report.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { numberWithCommas } from "../utils/utils";
 
 const Report = () => {
   const [totalMonth, setTotalMonth] = useState();
@@ -9,11 +11,34 @@ const Report = () => {
       .then((res) => setTotalMonth(res.data));
   }, []);
 
-  return (
+  return totalMonth ? (
     <div className="bg-red-50 rounded-sm shadow p-4 dark:bg-dark">
-      <p>
-        Chi tiêu:<span>{totalMonth.expense}</span>
-      </p>
+      <div className="grid grid-cols-2 gap-4 mb-2">
+        <div className="font-bold bg-white rounded-md p-2 flex justify-between">
+          <p>Chi tiêu:</p>
+          <p className="text-lossColor">
+            {numberWithCommas(totalMonth.expense)}
+          </p>
+        </div>
+
+        <div className="font-bold bg-white rounded-md p-2 flex justify-between items-center">
+          <p>Thu nhập:</p>
+          <p className="text-profitColor">
+            +{numberWithCommas(totalMonth.income)}
+          </p>
+        </div>
+      </div>
+
+      <div className="font-bold bg-white rounded-md p-2 flex justify-between">
+        <p>Thu chi:</p>
+        <p className="text-gray-600 text-right">
+          {numberWithCommas(totalMonth.balance)}
+        </p>
+      </div>
+    </div>
+  ) : (
+    <div className="bg-red-50 rounded-sm shadow p-4 dark:bg-dark">
+      Loading data...
     </div>
   );
 };
--- pages/api/transactions/totalMonth.js
+++ pages/api/transactions/totalMonth.js
@@ -3,7 +3,9 @@
 import axios from "axios";
 
 export default function totalMonth(req, res) {
+  // const url = `${process.env.BASE_URL}/transactions/by-category`;
   const url = `${process.env.BASE_URL}/transactions/by-category`;
+
   axios.get(url).then((response) => {
     res.status(200).json(response.data);
   });
